91548bfde1c28439bb692d3b6b9938766f66ed4c
Danilo Ramos <danilodramos@yahoo.com.br>
2022-12-26 16:36:01 -0300

    tratando erros com execao personalizadas

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -19,11 +19,44 @@ namespace tabuleiro
         {
             return pecas[linha, coluna];
         }
+        //criando uma sobrecarga no metodo peca
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+        //criando metodo testar
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         //colocando pecas no tabuleiro
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Ja Existe uma peca  nessa posicao! :");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+        //criando metodo para testar se posicao e valida
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha< 0 || pos.linha >=linhas || pos.coluna<0 || pos.coluna>= colunas){
+                return false;
+            }
+            return true;
+
+        }
+        //criando metodo para validar posicao
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posicao invalida!");
+            }
         }
 
     }
---
+++ xadrez-console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        //criando execao 
+        public TabuleiroException(string msg): base(msg)
+        {
+
+        }
+    }
+}
