fa5600f8ba8fcff2d2792c92750ddc5c328ac2f1
Danilo Ramos <danilodramos@yahoo.com.br>
2022-12-28 15:03:54 -0300

    acrescentando as demais pecas

---
+++ xadrez-console/xadrez/Peao.cs
@@ -0,0 +1,82 @@
+﻿using tabuleiro;
+namespace xadrez
+{
+    //classe Peao e uma subclasse da classe peca
+    class Peao : Peca
+    {
+        //criando objeto peao
+        public Peao(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+        }
+        //metodo tostring pro peao
+        public override string ToString()
+        {
+            return "P";
+        }
+        //metodo pode mover
+        private bool existeInimigo(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p != null || p.cor != cor;
+        }
+        private bool livre(Posicao pos)
+        {
+
+            return tab.peca(pos) == null;
+        }
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            if (cor == Cor.Branca)
+            {
+                pos.definirValores(posicao.linha - 1, posicao.coluna);
+                if (tab.posicaoValida(pos) && livre(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha - 2, posicao.coluna);
+                if (tab.posicaoValida(pos) && livre(pos) && qteMovimentos == 0)
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                else
+                {
+                    pos.definirValores(posicao.linha + 1, posicao.coluna);
+                    if (tab.posicaoValida(pos) && livre(pos))
+                    {
+                        mat[pos.linha, pos.coluna] = true;
+                    }
+                }
+                pos.definirValores(posicao.linha + 2, posicao.coluna);
+                if (tab.posicaoValida(pos) && livre(pos) && qteMovimentos == 0)
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+            }
+            return mat;
+        }
+    }
+}
